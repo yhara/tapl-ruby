ee310d45ab553710ef433c8d8e2657f7d694411e
Yutaka HARA <yutaka.hara.gmail.com>
2013-08-31 17:49:51 +0900

    Add parser

--- arith/arith.rb
+++ arith/arith.rb
@@ -11,75 +11,80 @@ Terms:
   [:IsZero, t1]
 =end
 
-class Arith
-  class NoRuleApplies < StandardError; end
+module Arith
+  class Evaluator
+    class NoRuleApplies < StandardError; end
 
-  def numeric_val?(t)
-    match(t) {
-      with(_[:Zero]) { true }
-      with(_[:Succ, t1]) { numeric_val?(t1) }
-      with(_){ false }
-    }
-  end
+    def numeric_val?(t)
+      match(t) {
+        with(_[:Zero]) { true }
+        with(_[:Succ, t1]) { numeric_val?(t1) }
+        with(_){ false }
+      }
+    end
 
-  def val?(t)
-    match(t) {
-      with(_[:True]) { true }
-      with(_[:False]) { true }
-      with(t, guard{ numeric_val?(t) }) { true }
-      with(_) { false }
-    }
-  end
+    def val?(t)
+      match(t) {
+        with(_[:True]) { true }
+        with(_[:False]) { true }
+        with(t, guard{ numeric_val?(t) }) { true }
+        with(_) { false }
+      }
+    end
 
-  def eval1(t)
-    match(t) {
-      with(_[:If, [:True], t2, t3]) {
-        t2
-      }
-      with(_[:If, [:False], t2, t3]) {
-        t3
-      }
-      with(_[:If, t1, t2, t3]) {
-        [:If, eval1(t1), t2, t3]
-      }
-      with(_[:Succ, t1]) { 
-        [:Succ, eval1(t1)]
-      }
-      with(_[:Pred, [:Zero]]) {
-        [:Zero]
-      }
-      with(_[:Pred, [:Succ, nv1]], guard{ numeric_val?(nv1)}) {
-        nv1
-      }
-      with(_[:Pred, t1]) {
-        [:Pred, eval1(t1)]
-      }
-      with(_[:IsZero, [:Zero]]) {
-        [:True]
-      }
+    def eval1(t)
+      match(t) {
+        with(_[:If, [:True], t2, t3]) {
+          t2
+        }
+        with(_[:If, [:False], t2, t3]) {
+          t3
+        }
+        with(_[:If, t1, t2, t3]) {
+          [:If, eval1(t1), t2, t3]
+        }
+        with(_[:Succ, t1]) { 
+          [:Succ, eval1(t1)]
+        }
+        with(_[:Pred, [:Zero]]) {
+          [:Zero]
+        }
+        with(_[:Pred, [:Succ, nv1]], guard{ numeric_val?(nv1)}) {
+          nv1
+        }
+        with(_[:Pred, t1]) {
+          [:Pred, eval1(t1)]
+        }
+        with(_[:IsZero, [:Zero]]) {
+          [:True]
+        }
 
-      # Note: this does not work (k-tsj/pattern-match#3)
-      # with(_[:IsZero, [:Succ, nv1]], guard{ numeric_val?(nv1)}) {
-      with(_[:IsZero, t1], guard{ t1[0] == :Succ && numeric_val?(t1[1]) }) {
-        [:False]
+        # Note: this does not work (k-tsj/pattern-match#3)
+        # with(_[:IsZero, [:Succ, nv1]], guard{ numeric_val?(nv1)}) {
+        with(_[:IsZero, t1], guard{ t1[0] == :Succ && numeric_val?(t1[1]) }) {
+          [:False]
+        }
+        
+        with(_[:IsZero, t1]) {
+          p [:IsZero3, t1: t1]
+          [:IsZero, eval1(t1)]
+        }
+        with(_) {
+          raise NoRuleApplies
+        }
       }
-      
-      with(_[:IsZero, t1]) {
-        p [:IsZero3, t1: t1]
-        [:IsZero, eval1(t1)]
-      }
-      with(_) {
-        raise NoRuleApplies
-      }
-    }
-  end
+    end
 
-  def eval(t)
-    eval(eval1(t))
-  rescue NoRuleApplies
-    t
+    def eval(t)
+      eval(eval1(t))
+    rescue NoRuleApplies
+      t
+    end
   end
 end
-p Arith.new.eval([:If, [:IsZero, [:Succ, [:Zero]]],
-                       [:Zero],
-                       [:Succ, [:Zero]]])
+
+if $0 == __FILE__
+  p Arith.new.eval([:If, [:IsZero, [:Succ, [:Zero]]],
+                         [:Zero],
+                         [:Succ, [:Zero]]])
+end
